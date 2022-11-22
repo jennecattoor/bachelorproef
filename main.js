7c116b2e97688470a70b54e28de6104197e322d9
Jenneeee <jenne@cattoor.net>
2022-11-22 14:28:02 +0100

    Changed file structure + added coloured squares

---
+++ main.js
@@ -0,0 +1,30 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="grid">
+  <div>
+    <h1>Column 1</h1>
+    <div class="square" id="square1"></div>
+  </div>
+    <div>
+    <h1>Column 2</h1>
+    <div class="square" id="square2"></div>
+  </div>
+    <div>
+    <h1>Column 3</h1>
+    <div class="square" id="square3"></div>
+  </div>
+    <div>
+    <h1>Column 4</h1>
+    <div class="square" id="square4"></div>
+  </div>
+    <div>
+    <h1>Column 5</h1>
+    <div class="square" id="square5"></div>
+  </div>
+    <div>
+    <h1>Column 6</h1>
+    <div class="square" id="square6"></div>
+  </div>
+  </div>
+`
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Devine!</h1>
-  </div>
-`
