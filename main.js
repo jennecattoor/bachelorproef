6c2c6cb50d372c45ac3162b959a4deebb7ac8a4e
Jenneeee <jenne@cattoor.net>
2022-11-23 09:34:38 +0100

    Added Array of Songs

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 import './style.css'
 import './reset.css'
 
+const songs = [
+  { name: `Take On Me`, band: `a-ha`, src: `./public/songs/takeonme.mp3`, bpm: 84 },
+  { name: `Hold the Line`, band: `TOTO`, src: ``, bpm: 97 },
+  { name: `Your Love`, band: `The Outfield`, src: ``, bpm: 130 },
+  { name: `I'm Still Standing`, band: `The Outfield`, src: ``, bpm: 88 },
+  { name: `Midnight City`, band: `M83`, src: ``, bpm: 105 },
+  { name: `Dancing Queen`, band: `ABBA`, src: ``, bpm: 101 },
+  { name: `I Wanne Dance with Somebody`, band: `Whitney Houston`, src: ``, bpm: 119 },
+  { name: `Got My Mind Set On You`, band: `George Harrison`, src: ``, bpm: 149 },
+  { name: `I'm Gonne Be`, band: `The Proclaimers`, src: ``, bpm: 118 },
+  { name: `September`, band: `Earth, Wind & Fire`, src: ``, bpm: 126 },
+  { name: `I Want You Back`, band: `The Jackson 5`, src: ``, bpm: 98 },
+  { name: `Help!`, band: `The Beatles`, src: ``, bpm: 95 },
+  { name: `Don't Stop Me Now`, band: `Queen`, src: ``, bpm: 156 },
+  { name: `Great Balls Of Fire`, band: `Jerry Lee Lewis`, src: ``, bpm: 79 },
+  { name: `You're The One That I Want`, band: `John Travolta, Olivia Newton-John`, src: ``, bpm: 107 },
+  { name: `Serotonin`, band: `Tom Walker`, src: ``, bpm: 164 }]
+
+const totalJumps = 180
+
 document.querySelector('#app').innerHTML = `
   <div class="grid">
     <div class="grid-column">
